e28a40d2e08c2ff6616891290039753b0539e7bd
Kaarlo Sasiang <sasiang64@gmail.com>
2024-04-01 23:05:02 +0800

    project initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
 };
